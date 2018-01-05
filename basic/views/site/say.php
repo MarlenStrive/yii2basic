624de7adce991a52578020d63494519a74bc420d
MarlenStrive <marlenstrive@gmail.com>
2018-01-05 14:41:21 +0000

    first commit of hello world for yii2

---
+++ basic/views/site/say.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Say';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<h1>Hello <?= Html::encode($target) ?></h1>
+<p>Welcome to your Yii2 demonstration application.</p>
